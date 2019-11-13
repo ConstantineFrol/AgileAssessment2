882cff764a8018647b7ce774c72a856738f21988
Konstantin Frolov <konstantin.frolov@mycit.ie>
2019-11-13 20:47:57 +0000

    basic Python file

---
+++ HelloPython.py
@@ -0,0 +1,11 @@
+import datetime
+import socket
+print("Hello world!")
+now = datetime.datetime.now()
+print ("Current date and time is ")
+print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+
+socket.gethostbyname(socket.gethostname())
+
+
+	
