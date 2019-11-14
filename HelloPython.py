9af55e82f5ff5d3df0c012b20623156c1fc61efa
Konstantin Frolov <konstantin.frolov@mycit.ie>
2019-11-14 23:56:56 +0000

    Python Script: Get Geo Location By IP

--- HelloPython.py
+++ HelloPython.py
@@ -1,13 +1,39 @@
+#!/bin/python
+
 import datetime
 import socket
+import re
+import sys
+import urllib2
+import BeautifulSoup
+
+
 print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 
-print ("Constatine with a C or K")
+# Get the Geo Location of an IP Address
 
-socket.gethostbyname(socket.gethostname())
+usage = "Run the script: ./geolocate.py IPAddress"
+
+if len(sys.argv)!=2:
+    print(usage)
+    sys.exit(0)
+
+if len(sys.argv) > 1:
+    ipaddr = sys.argv[1]
+
+geody = "http://www.geody.com/geoip.php?ip=" + ipaddr
+html_page = urllib2.urlopen(geody).read()
+soup = BeautifulSoup.BeautifulSoup(html_page)
+
+# Filter paragraph containing geolocation info.
+paragraph = soup('p')[3]
+
+# Remove html tags using regex.
+geo_txt = re.sub(r'<.*?>', '', str(paragraph))
+print geo_txt[32:].strip()
 
 
 	
