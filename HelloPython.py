1066e2232c5cd07228d3fccd9b1de0eeeb133032
Konstantin Frolov <konstantin.frolov@mycit.ie>
2019-11-15 00:14:45 +0000

    Python Script: start from scratch

--- HelloPython.py
+++ HelloPython.py
@@ -1,40 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 import datetime
-import socket
-import re
-import sys
-import urllib2
-import BeautifulSoup
 
-
-print("Hello world!")
 now = datetime.datetime.now()
-print ("Current date and time is ")
-print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 
-# Get the Geo Location of an IP Address
+print
+print "Current date and time using str method of datetime object:"
+print str(now)
 
-usage = "Run the script: ./geolocate.py IPAddress"
+print
+print "Current date and time using instance attributes:"
+print "Current year: %d" % now.year
+print "Current month: %d" % now.month
+print "Current day: %d" % now.day
+print "Current hour: %d" % now.hour
+print "Current minute: %d" % now.minute
+print "Current second: %d" % now.second
+print "Current microsecond: %d" % now.microsecond
 
-if len(sys.argv)!=2:
-    print(usage)
-    sys.exit(0)
-
-if len(sys.argv) > 1:
-    ipaddr = sys.argv[1]
-
-geody = "http://www.geody.com/geoip.php?ip=" + ipaddr
-html_page = urllib2.urlopen(geody).read()
-soup = BeautifulSoup.BeautifulSoup(html_page)
-
-# Filter paragraph containing geolocation info.
-paragraph = soup('p')[3]
-
-# Remove html tags using regex.
-geo_txt = re.sub(r'<.*?>', '', str(paragraph))
-print geo_txt[32:].strip()
-
-
-	
-# This line was made by Konstantin
+print
+print "Current date and time using strftime:"
+print now.strftime("%Y-%m-%d %H:%M")
