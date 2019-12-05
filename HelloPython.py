bc1e56560d90c5602490bb97093bed126d295b5d
GOG <gogorman@mac.com>
2019-12-05 20:55:38 +0000

    changed password if not in user statement

--- HelloPython.py
+++ HelloPython.py
@@ -1,56 +1,112 @@
 #!/usr/bin/python
-#R00131068gghfe
+# R00131068
+
 import datetime
+import os
 import re
 import sys
-import os
-# Trigger a remote build
-statTime = 8
+
+# Global variables
+from numpy import var
+
 now = datetime.datetime.now()
-# Did i go to Jenkins f
+user = []
 
-print
-print ("Current date and time using str method of datetime object:")
-print (str(now))
 
-print
-print ("Current date and time using instance attributes:")
-print ("Current year: %d" % now.year)
-print ("Current month: %d" % now.month)
-print ("Current day: %d" % now.day)
-print ("Current hour: %d" % now.hour)
-print ("Current minute: %d" % now.minute)
-print ("Current second: %d" % now.second)
-print ("Current microsecond: %d" % now.microsecond)
+# Menu
+def displayMenu():
+    ans = True
+    while ans:
+        print("""
+        1.Register
+        2.Clock in
+        3.Exit/Quit
+        """)
+        ans = input("What would you like to do? ")
+        if ans == "1":
+            registrationProcess()
+        elif ans == "2":
+            print("")
+            clockInProcess()
+        elif ans == "3":
+            print("\n Goodbye")
+            ans = False
+        else:
+            print("\n Not Valid Choice Try again")
 
-print
-print ("Current date and time using strftime:")
-print (now.strftime("%Y-%m-%d %H:%M"))
 
-# Create file and write data
+# End Menu
 
-filepath = '/Users/gog/Documents/GitHub/AgileAssessment2/ClockingMachine/shifts.txt'
+# Registration Process
+def registrationProcess():
+    complete = False
+    while not complete:
+        username = input("Please type in username\n")
+        if username not in user:
+            password = input("Please type in password\n")
+        else:
+            print("! User with such name Already Exist !\n")
+            continue
+        conf_username = input("Repeat the username?\n")
+        conf_password = input("Repeat the password?\n")
 
-mode = 'a' if os.path.exists(filepath) else 'w'
-with open(filepath, mode) as f:
-	if now.hour == statTime:
-		f.write(now.strftime("%Y-%m-%d %H:%M(start work)\n"))
-	elif (now.hour == statTime and now.minute >= 1):
-		print ("You are late today")
-		f.write(now.strftime("%Y-%m-%d %H:%M (late)\n"))
-	elif now.hour >= (statTime + 6) and now.hour < (statTime + 7):
-		print ("See you after lunch")
-		f.write(now.strftime("%Y-%m-%d %H:%M (lunch time)\n"))
-	elif (now.hour == (statTime + 7) and now.minute <= 5):
-		print ("Welcome back")
-		f.write(now.strftime("%Y-%m-%d %H:%M (start after lunch)\n"))
-	elif (now.hour == (statTime + 7) and now.minute >= 5):
-		print ("Hurry up")
-		f.write(now.strftime("%Y-%m-%d %H:%M (late)\n"))
-	elif (now.hour == (statTime + 9) and now.minute <= 5):
-		print ("See you tomorrow")
-		f.write(now.strftime("%Y-%m-%d %H:%M (end work)\n"))
-	elif now.hour > (statTime + 9):
-		print ("Go home !!!")
-		f.write(now.strftime("%Y-%m-%d %H:%M (end work + owertimes)\n"))
-f.close()
+        if username != conf_username or password != conf_password:
+            print("username or password does not match\n")
+            continue
+        if username in user:
+            print("User has been identified, Welcome", username)
+            complete = True
+        else:
+            user.append("User: " + username)
+            user.append("Password: " + password)
+            print("User created successfully\n")
+            for usr in user:
+                print(usr)
+            displayMenu()
+
+
+# End Registration Process
+
+# Clocking in Process
+def clockInProcess():
+    statTime = 8
+    if now.hour < statTime:
+        print(now.hour)
+    elif now.hour == statTime:
+        storeDataProcess("%Y-%m-%d %H:%M")
+    elif (now.hour == statTime and now.minute >= 1):
+        print(now.hour)
+        storeDataProcess("%Y-%m-%d %H:%M")
+    elif now.hour >= (statTime + 6) and now.hour < (statTime + 7):
+        print(now.hour)
+        storeDataProcess("%Y-%m-%d %H:%M")
+    elif (now.hour == (statTime + 7) and now.minute <= 5):
+        print(now.hour)
+        storeDataProcess("%Y-%m-%d %H:%M")
+    elif (now.hour == (statTime + 7) and now.minute >= 5):
+        print(now.hour)
+        storeDataProcess("%Y-%m-%d %H:%M")
+    elif (now.hour == (statTime + 9) and now.minute <= 5):
+        print(now.hour)
+        storeDataProcess("%Y-%m-%d %H:%M")
+    elif now.hour > (statTime + 9):
+        print(now.hour)
+        storeDataProcess("%Y-%m-%d %H:%M")
+
+
+# End of Clocking Process
+
+# Store Records on File
+def storeDataProcess(var):
+    filepath = '/Users/gog/Documents/GitHub/AgileAssessment2/ClockingMachine/shifts.txt'
+    mode = 'a' if os.path.exists(filepath) else 'w'
+    with open(filepath, mode) as f:
+        f.write(now.strftime(var + "\n"))
+        f.close()
+
+
+# End Store Records on File
+
+displayMenu()
+
+print("The End")
